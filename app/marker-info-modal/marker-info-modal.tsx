f578397c985034ad07ff6847e7bc8561354c8dde
Mikołaj Wiśniewski <javadreamsmikolaj@gmail.com>
2024-06-27 14:05:09 +0200

    feat: change the way info modal is displayed

--- app/marker-info-modal/marker-info-modal.tsx
+++ app/marker-info-modal/marker-info-modal.tsx
@@ -30,30 +30,32 @@ export const MarkerInfoModal = () => {
 		}
 	}, [appState.markerInfoModal.isShown, handleMarkerInfoModalHide])
 
-	return (
-		<div className={`absolute bottom-0 z-800 p-4 transition-all left-1/2 -translate-x-1/2 ${toggleInfoModalStyles}`} ref={modalRef}>
-			<Box className="flex flex-col h-full w-72 sm:w-96">
-				<div
-					className={`flex justify-between ${
-						categoryToAssets[appState.markerInfoModal.survey?.category as string]?.gradient
-					} -m-[2px] rounded-ss-lg rounded-se-lg p-4 text-white`}>
-					<h3 className="font-bold">{translations.category[appState.markerInfoModal.survey?.category as Category]}</h3>
-				</div>
-				<div className="w-full flex flex-col p-4">
-					<div>
-						<small className="font-bold text-gray">{translations.pointDetails.placeName}</small>
-						<h4 className="font-bold text-lg">{appState.markerInfoModal.survey?.location.name}</h4>
+	if (appState.markerInfoModal.isShown) {
+		return (
+			<div className={`absolute bottom-0 z-800 p-4 transition-all left-1/2 -translate-x-1/2 ${toggleInfoModalStyles}`} ref={modalRef}>
+				<Box className="flex flex-col h-full w-72 sm:w-96">
+					<div
+						className={`flex justify-between ${
+							categoryToAssets[appState.markerInfoModal.survey?.category as string]?.gradient
+						} -m-[2px] rounded-ss-lg rounded-se-lg p-4 text-white`}>
+						<h3 className="font-bold">{translations.category[appState.markerInfoModal.survey?.category as Category]}</h3>
 					</div>
-					<div>
-						<small className="font-bold text-gray">{translations.pointDetails.problemDescription}</small>
-						<p className="text-xs">{appState.markerInfoModal.survey?.description}</p>
+					<div className="w-full flex flex-col p-4">
+						<div>
+							<small className="font-bold text-gray">{translations.pointDetails.placeName}</small>
+							<h4 className="font-bold text-lg">{appState.markerInfoModal.survey?.location.name}</h4>
+						</div>
+						<div>
+							<small className="font-bold text-gray">{translations.pointDetails.problemDescription}</small>
+							<p className="text-xs">{appState.markerInfoModal.survey?.description}</p>
+						</div>
+						<div>
+							<small className="font-bold text-gray">{translations.pointDetails.problemSolution}</small>
+							<p className="text-xs">{appState.markerInfoModal.survey?.solution}</p>
+						</div>
 					</div>
-					<div>
-						<small className="font-bold text-gray">{translations.pointDetails.problemSolution}</small>
-						<p className="text-xs">{appState.markerInfoModal.survey?.solution}</p>
-					</div>
-				</div>
-			</Box>
-		</div>
-	)
+				</Box>
+			</div>
+		)
+	}
 }
